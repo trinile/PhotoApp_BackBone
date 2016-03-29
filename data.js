cf433e0a49b9350b3b521c4a3569a9e2d4603a51
Trini Le <trinile@gmail.com>
2016-03-29 16:24:14 -0700

    Forgot to add images folder! woops

--- data.js
+++ data.js
@@ -1,26 +1,26 @@
 var photos = [
   {
-    url: '../images/BigTreeArea.jpg',
+    url: 'images/BigTreeArea.jpg',
     title: 'Big Tree Area',
     rating: 4,
   },
   {
-    url: '../images/BrightAngel.jpg',
+    url: 'images/BrightAngel.jpg',
     title: 'Walking up Bright Angel Trail in the Snow',
     rating: 5,
   },
   {
-    url: '../images/Bryce.jpg',
+    url: 'images/Bryce.jpg',
     title: 'Bryce Canyon at sunset in December',
     rating: 4,
   },
   {
-    url: '../images/GrandCanyon.jpg',
+    url: 'images/GrandCanyon.jpg',
     title: 'South Kaibab Trail down to Grand Canyon',
     rating: 5,
   },
   {
-    url: '../images/NewHampshire.jpg',
+    url: 'images/NewHampshire.jpg',
     title: 'Winter Wonderland in Mt. Washington area',
     rating: 4,
   }
